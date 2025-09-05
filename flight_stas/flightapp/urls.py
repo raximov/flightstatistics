4b7f2fc0065e270c6a3b54bf90c317b96d52cc3b
volentib <volentib@student.21-school.ru>
2025-09-05 18:16:01 +0500

    updated django ORM

--- flight_stas/flightapp/urls.py
+++ flight_stas/flightapp/urls.py
@@ -1,11 +1,10 @@
 # flightapp/urls.py
 from django.urls import path
-from .views import  FlightStatisticsSQL, FlightStatisticsAPIView, FlightStatisticsAPIView2, FlightStatisticsAPIView4
+from .views import  FlightStatisticsSQL, FlightStatisticsAPIView, FlightStatisticsAPIView2
 urlpatterns = [
    
     path('stats1/', FlightStatisticsSQL.as_view()),
     path('stats2/', FlightStatisticsAPIView.as_view()),
     path('stats3/', FlightStatisticsAPIView2.as_view()),
-    path('stats4/', FlightStatisticsAPIView4.as_view()),
 
 ]
